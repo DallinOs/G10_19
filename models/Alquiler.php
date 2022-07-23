dc6ebdc9595914953613281b84ad3d31d7198d84
LuesbelinMejia <109258635+LuesbelinMejia@users.noreply.github.com>
2022-07-23 15:06:04 -0600

    Correción conexion,models y controller de Alquiler

--- models/Alquiler.php
+++ models/Alquiler.php
@@ -1,5 +1,62 @@
 <?php
-    if(){
-        
+    class Alquiler extends Conectar{
+        public function get_Alquiler(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM Alquiler";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_Alquilers($CodigoDeLibro){
+            $conectar=parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM Alquiler WHERE CodigoDeLibro=?";
+            $sql=$conectar->prepare($sql);
+            $sql->binValue(1,$CodigoDeLibro);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_alquiler($CodigoDeLibro,$NombreLibro,$FechaAlqui,$NombreCliente,$Direccion,$DiasAlqui,$PrecioAlqui){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="INSERT INTO Alquiler (CodigoDeLibro,NombreLibro,FechaAlqui,NombreCliente,Direccion,DiasAlqui,PrecioAlqui)
+            VALUES (?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$CodigoDeLibro);
+            $sql->bindValue(2,$NombreLibro);
+            $sql->bindValue(3,$FechaAlqui);
+            $sql->bindValue(4,$NombreCliente);
+            $sql->bindValue(5,$Direccion);
+            $sql->bindValue(6,$DiasAlqui);
+            $sql->bindValue(7,$PrecioAlqui);
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_alquiler($CodigoDeLibro,$NombreLibro,$FechaAlqui,$NombreCliente,$Direccion,$DiasAlqui,$PrecioAlqui){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="UPDATE Alquiler set CodigoDeLibro=$CodigoDeLibro, NombreLibro=$NombreLibro, FechaAlqui=$FechaAlqui, NombreCliente=$NombreCliente, Direccion=$Direccion, DiasAlqui=$DiasAlqui, PrecioAlqui=$PrecioAlqui WHERE CodigoDeLibro=$CodigoDeLibro ;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$CodigoDeLibro);
+            $sql->bindValue(2,$NombreLibro);
+            $sql->bindValue(3,$FechaAlqui);
+            $sql->bindValue(4,$NombreCliente);
+            $sql->bindValue(5,$Direccion);
+            $sql->bindValue(6,$DiasAlqui);
+            $sql->bindValue(7,$PrecioAlqui);
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_alquiler($CodigoDeLibro){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="DELETE FROM Alquiler  WHERE CodigoDeLibro=$CodigoDeLibro ;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$CodigoDeLibro);
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 ?>
