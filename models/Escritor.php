3095709a5e4d0ce47910f56aab0118ed6e7cf3ea
SLara26 <seydi.lara@unah.hn>
2022-07-23 15:09:18 -0600

    Creación de los metodos para entidad escritor.

--- models/Escritor.php
+++ models/Escritor.php
@@ -1,7 +1,73 @@
 <?php
 
     class Escritor extends Conectar{
+
+        public function get_escritor(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM escritor";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_escritores(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM escritor WHERE ID=?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$NumeroEscritor);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
         
+        public function insert_escritor($NumeroEscritor,$NombreEscritor,$ApellidosEscritor,$FechaNacimiento,$Nacionalidad,$CantidadLibrosEscritos,$Email){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="INSERT INTO Escritor (NumeroEscritor,NombreEscritor,ApellidosEscritor,FechaNacimiento,Nacionalidad,CantidadLibrosEscritos,Email)
+            VALUES (?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$NumeroEscritor);
+            $sql->bindValue(2,$NombreEscritor);
+            $sql->bindValue(3,$ApellidosEscritor);
+            $sql->bindValue(4,$FechaNacimiento);
+            $sql->bindValue(5,$Nacionalidad);
+            $sql->bindValue(6,$CantidadLibrosEscritos);
+            $sql->bindValue(7,$Email);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_escritor($NumeroEscritor,$NombreEscritor,$ApellidosEscritor,$FechaNacimiento,$Nacionalidad,$CantidadLibrosEscritos,$Email){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="UPDATE Escritor SET NumeroEscritor=$NumeroEscritor, NombreEscritor=$NombreEscritor, ApellidosEscritor=$ApellidosEscritor, 
+                    FechaNacimiento=$FechaNacimiento, Nacionalidad=$Nacionalidad, CantidadLibrosEscritos=$CantidadLibrosEscritos, 
+                    Email=$Email WHERE NumeroEscritor=$NumeroEscritor ;";
+            
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$NumeroEscritor);
+            $sql->bindValue(2,$NombreEscritor);
+            $sql->bindValue(3,$ApellidosEscritor);
+            $sql->bindValue(4,$FechaNacimiento);
+            $sql->bindValue(5,$Nacionalidad);
+            $sql->bindValue(6,$CantidadLibrosEscritos);
+            $sql->bindValue(7,$Email);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_escritor($NumeroEscritor){
+            $conectar= parent:: conexion();
+            parent::set_name();
+            $sql="DELETE FROM Escritor WHERE NumeroEscritor=$NumeroEscritor ;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1,$NumeroEscritor);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
     }
 
 ?>
