934768025483e133ce21a0b5e00d1b1c82d54195
LuesbelinMejia <109258635+LuesbelinMejia@users.noreply.github.com>
2022-07-23 11:45:48 -0600

    Creación de la conexion

--- config/conexion.php
+++ config/conexion.php
@@ -1,6 +1,19 @@
 <?php 
     class Conectar{
-        
-    }
 
+        protected $dbh;
+        protected funtion Conexion(){
+        try {
+            $conenctar = $this->dbh = new PDO("mysql: host=20.216.41.245; dbname=g10_19", "G10_19"," r6rQPhUq");
+            return $conectar;
+        } catch (Exception $e) {
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+        }
+
+        public function set_names(){
+            return $this->dbh->query("SET NAME 'UTF8'");
+        }
+    }
 ?>
