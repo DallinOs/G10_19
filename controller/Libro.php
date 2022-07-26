a8ff8e8f2d1d2a621b80fb82133f8893aa17fdd7
Dallin Eleazar Osorio Cruz <109120748+DallinOs@users.noreply.github.com>
2022-07-25 22:02:51 -0600

    Cambios en models y controller para Libro

--- controller/Libro.php
+++ controller/Libro.php
@@ -1,47 +1,49 @@
 <?php
-
-   if($_SERVER['REQUEST_METHOD']==='OPTIONS'){
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPCIONS');
-        header('Access-Control-Allow-Headers: token, Content-Type');
-        header('Access-Control-Allow-Max-Age: 1728000');
-        header('Content-Length: 0');
-        header('Content-Type: text/plain');
-        die();
-    }
-
+  if($_SERVER['REQUEST_METHOD']==='OPTIONS'){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPCIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Allow-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
     require_once("../config/conexion.php");
-    require_once("../models/Alquiler.php");
-    $Libro = new libro();
+    require_once("../models/Libro.php");
+
+    $Libro= new Libros();
 
     $body=json_decode(file_get_contents("php://input"),true);
 
-    switch ($GET["op"]) {
-        case 'Get_libros':
-            $datos = $Libro-> Get_libros();
-            echo json_encode($datos);
-            break;
 
-        case 'Get_libro':
+    switch($_GET["op"]){
+        case"Get_libros":
+            $datos=$Libro->Get_libros();
+            echo json_encode($datos);
+        break;
+
+        case "Get_libro":
             $datos = $Libro-> Get_libro($body["CodigoDeLibro"]);
             echo json_encode($datos);
-            break;
+        break;
         
-        case 'Insert_libro':
+        case "Insert_libro":
             $datos = $Libro-> Insert_libro($body["CodigoDeLibro"],$body["NombreLibro"],$body["NombreEscritor"],$body["FechaPublicacion"],$body["ISBN"],$body["Precio"],$body["Editorial"]);
             echo json_encode($datos);
-            break;
+        break;
     
-        case 'Update_libro':
+        case "Update_libro":
             $datos = $Libro-> Update_libro($body["CodigoDeLibro"],$body["NombreLibro"],$body["NombreEscritor"],$body["FechaPublicacion"],$body["ISBN"],$body["Precio"],$body["Editorial"]);
             echo json_encode($datos);
-            break;
+        break;
 
-        case 'Delete_libro':
+        case "Delete_libro":
             $datos = $Libro-> Delete_libro($body["CodigoDeLibro"]);
             echo json_encode($datos);
-            break;
+        break;
     }
+
+?>
--- models/Libro.php
+++ models/Libro.php
@@ -1,64 +1,69 @@
 <?php
 
-class Libro extends conectar{
+    class Libros extends conectar{
 
-    public function get_libros(){
-        $conectar = parent::conexion();
-        parent:: set_names();
-        $sql = "SELECT * FROM Libro";
-        $sql = $conectar->prepare($sql);
-        $sql->execute();
-        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        public function get_libros(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM libro;";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+       
+      
+
+        public function get_libro ($CodigoDeLibro){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql = "SELECT * FROM Libro WHERE CodigoDeLibro = ?";
+            $sql = $conectar->prepare($sql);
+            $sql -> bindValue(1,$CodigoDeLibro);
+            $sql->execute();
+            return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_libro ($CodigoDeLibro, $NombreLirbro, $NombreEscritor, $FechaPublicacion, $ISBN, $Precio, $Editorial){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql = "INSERT INTO Libro(CodigoDeLibro,NombreLibro,NombreEscritor,FechaPublicacion,ISBN,Precio,Editorial) Values (?,?,?,?,?,?,?)";
+            $sql = $conectar->prepare($sql);
+            $sql -> bindValue(1,$CodigoDeLibro);
+            $sql -> bindValue(2,$NombreLirbro);
+            $sql -> bindValue(3,$NombreEscritor);
+            $sql -> bindValue(4,$FechaPublicacion);
+            $sql -> bindValue(5,$ISBN);
+            $sql -> bindValue(6,$Precio);
+            $sql -> bindValue(7,$Editorial);
+            $sql->execute();
+            return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_libro ($CodigoDeLibro,$NombreLibro,$NombreEscritor,$FechaPublicacion,$ISBN,$Precio,$Editorial){
+            $conectar = parent::conexion();
+            parent:: set_names();
+            $sql = "UPDATE Libro SET CodigoDeLibro=?,NombreLibro=?,NombreEscritor=?,FechaPublicacion=?,ISBN=?,Precio=?,Editorial=? WHERE CodigoDeLibro =$CodigoDeLibro";
+            $sql = $conectar->prepare($sql);
+            $sql -> bindValue(1,$CodigoDeLibro);
+            $sql -> bindValue(2,$NombreLibro);
+            $sql -> bindValue(3,$NombreEscritor);
+            $sql -> bindValue(4,$FechaPublicacion);
+            $sql -> bindValue(5,$ISBN);
+            $sql -> bindValue(6,$Precio);
+            $sql -> bindValue(7,$Editorial);
+            $sql->execute();
+            return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_libro ($CodigoDeLibro){
+            $conectar = parent::conexion();
+            parent:: set_names();
+            $sql = "DELETE FROM Libro WHERE CodigoDeLibro =?";
+            $sql = $conectar->prepare($sql);
+            $sql -> bindValue(1,$CodigoDeLibro);
+            $sql->execute();
+            return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 
-    public function get_libro ($CodigoDeLibro){
-        $conectar = parent::conexion();
-        parent:: set_names();
-        $sql = "SELECT * FROM Libro WHERE CodigoDeLibro = ?";
-        $sql = $conectar->prepare($sql);
-        $sql -> bindValue(1,$CodigoDeLibro);
-        $sql->execute();
-        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function insert_libro ($CodigoDeLibro,$NombreLirbro,$NombreEscritor,$FechaPublicacion,$ISBN,$Precio,$Editorial){
-        $conectar = parent::conexion();
-        parent:: set_names();
-        $sql = "INSERT INTO Libro(CodigoDeLibro,$}NombreLirbro,NombreEscritor,FechaPublicacion,ISBN,Precio,Editorial) Values (?,?,?,?,?,?,?)";
-        $sql = $conectar->prepare($sql);
-        $sql -> bindValue(1,$CodigoDeLibro);
-        $sql -> bindValue(2,$NombreLirbro);
-        $sql -> bindValue(3,$NombreEscritor);
-        $sql -> bindValue(4,$FechaPublicacion);
-        $sql -> bindValue(5,$ISBN);
-        $sql -> bindValue(6,$Precio);
-        $sql -> bindValue(7,$Editorial);
-        $sql->execute();
-        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function update_libro ($CodigoDeLibro,$NombreLirbro,$NombreEscritor,$FechaPublicacion,$ISBN,$Precio,$Editorial){
-        $conectar = parent::conexion();
-        parent:: set_names();
-        $sql = "UPDATE Libro SET NombreLirbro=?,NombreEscritor=?,FechaPublicacion=?,ISBN=?,Precio=?,Editorial=? WHERE CodigoDeLibro =$CodigoDeLibro";
-        $sql = $conectar->prepare($sql);
-        $sql -> bindValue(1,$NombreLirbro);
-        $sql -> bindValue(2,$NombreEscritor);
-        $sql -> bindValue(3,$FechaPublicacion);
-        $sql -> bindValue(4,$ISBN);
-        $sql -> bindValue(5,$Precio);
-        $sql -> bindValue(6,$Editorial);
-        $sql->execute();
-        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function delete_libro ($CodigoDeLibro){
-        $conectar = parent::conexion();
-        parent:: set_names();
-        $sql = "DELETE LIBRO WHERE CodigoDeLibro =?";
-        $sql = $conectar->prepare($sql);
-        $sql -> bindValue(1,$CodigoDeLibro);
-        $sql->execute();
-        return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
+?>
